2b137eeed70e412662f5059f773c7dea509a71d5
antonkonsta <148660379+antonkonsta@users.noreply.github.com>
2023-11-08 21:30:30 -0500

    modified:   Cd_charts.py

--- Cd_charts.py
+++ Cd_charts.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 # Constants and Parameters (SI Units)
 g = 9.81  # Acceleration due to gravity (m/s^2)
 rho = 1.225  # Air density (kg/m^3)
-A = 0.0108528815  # Cross-sectional area of the nosecone (m^2)
-m = 4.536  # Mass of the nosecone (kg)
+A = 0.0108528815  # Cross-sectional area of the nosecone (m^2)    
+m = 4.536  # Mass of the nosecone (kg)wddwad
 
 # Range of Cd values 
 Cd_values = np.linspace(0.2, 0.4, 100)
@@ -14,7 +14,7 @@ Cd_values = np.linspace(0.2, 0.4, 100)
 terminal_velocities = np.sqrt((2 * m * g) / (rho * A * Cd_values))
 
 # Calculate kinetic energy for each Cd value
-kinetic_energies = 0.5 * m * terminal_velocities**2
+kinetic_energies = 0.5 * m * terminal_velocities**2  
 
 # Specific Cd values and their corresponding terminal velocities and kinetic energies
 specific_Cd_values = [0.28, 0.35, 0.3088]  # Add the Cd values you want to highlight
